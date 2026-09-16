3ebba3cddf1a9b166314890b5ae6f9fe3c2d35dd
SvetoiDen <geymerworld123@gmail.com>
2026-09-16 16:49:17 +0300

    edit test.py

--- task_1/test.py
+++ task_1/test.py
@@ -1,7 +1,7 @@
 from code_task import *
 import time
 
-sizes = [8, 32, 128, 512, 1024, 4096, 16384, 65536]
+sizes = [8, 32, 128, 512, 1024, 4096, 16384, 65536, 262144]
 sep_print = "# ==================================== #"
 print(sep_print)
 print("# Тестирование и время сдвига масссива на d позиции #")
@@ -12,10 +12,10 @@ d = 3
 
 for n in sizes:
     a = list(range(n))
-    t1 = time.time()
+    t1 = time.perf_counter_ns()
     res = swift_temp(a, d)
-    t2 = time.time()
-    print(f"n = {n:>4} | время = {(t2 - t1) * 1000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+    t2 = time.perf_counter_ns()
+    print(f"n = {n:>6} | время = {(t2 - t1)/1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
 
 print(sep_print)
 print(f"# Второй способ при {sizes} элементах массива при d=6 #")
@@ -24,10 +24,10 @@ d = 6
 
 for n in sizes:
     a = list(range(n))
-    t1 = time.time()
+    t1 = time.perf_counter_ns()
     res = swift_loop(a, d)
-    t2 = time.time()
-    print(f"n = {n:>4} | время = {(t2 - t1) * 1000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+    t2 = time.perf_counter_ns()
+    print(f"n = {n:>6} | время = {(t2 - t1)/1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
 
 print(sep_print)
 print(f"# Третий способ при {sizes} элементах массива при d=8 #")
@@ -36,9 +36,9 @@ d = 8
 
 for n in sizes:
     a = list(range(n))
-    t1 = time.time()
+    t1 = time.perf_counter_ns()
     res = swift_swift(a, d)
-    t2 = time.time()
-    print(f"n = {n:>4} | время = {(t2 - t1) * 1000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+    t2 = time.perf_counter_ns()
+    print(f"n = {n:>6} | время = {(t2 - t1)/1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
 
 print(sep_print)
