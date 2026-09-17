86baf65c7825b64393439a90c1c14bec8111ea47
SvetoiDen <geymerworld123@gmail.com>
2026-09-17 10:08:23 +0300

    edit and add func "tests.py" в "task 1"

--- task_1/test.py
+++ task_1/test.py
@@ -2,43 +2,93 @@ from code_task import *
 import time
 
 sizes = [8, 32, 128, 512, 1024, 4096, 16384, 65536, 262144]
-sep_print = "# ==================================== #"
-print(sep_print)
-print("# Тестирование и время сдвига масссива на d позиции #")
-print(sep_print)
-print(f"# Первый способ при {sizes} элементах массива d=3 #")
 
-d = 3
 
-for n in sizes:
-    a = list(range(n))
-    t1 = time.perf_counter_ns()
-    res = swift_temp(a, d)
-    t2 = time.perf_counter_ns()
-    print(f"n = {n:>6} | время = {(t2 - t1)/1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+def main_test(sizes):
+    jsonData = {}
 
-print(sep_print)
-print(f"# Второй способ при {sizes} элементах массива при d=6 #")
+    sep_print = "# ==================================== #"
+    print(sep_print)
+    print("# Тестирование и время сдвига масссива на d позиции #")
+    print(sep_print)
+    print(f"# Первый способ при {sizes} элементах массива d=3 #")
 
-d = 6
+    d = 3
 
-for n in sizes:
-    a = list(range(n))
-    t1 = time.perf_counter_ns()
-    res = swift_loop(a, d)
-    t2 = time.perf_counter_ns()
-    print(f"n = {n:>6} | время = {(t2 - t1)/1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+    tempList = {}
+    for n in sizes:
+        a = list(range(n))
+        t1 = time.perf_counter_ns()
+        res = swift_temp(a, d)
+        t2 = time.perf_counter_ns()
+        tempList[f"{n}"] = f"{(t2 - t1)}"
+        print(f"n = {n:>6} | время = {(t2 - t1) / 1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+    jsonData['first_swift'] = tempList
 
-print(sep_print)
-print(f"# Третий способ при {sizes} элементах массива при d=8 #")
+    print(sep_print)
+    print(f"# Второй способ при {sizes} элементах массива при d=12 #")
 
-d = 8
+    d = 12
+    tempList = {}
+    for n in sizes:
+        a = list(range(n))
+        t1 = time.perf_counter_ns()
+        res = swift_loop(a, d)
+        t2 = time.perf_counter_ns()
+        tempList[f"{n}"] = f"{(t2 - t1)}"
+        print(f"n = {n:>6} | время = {(t2 - t1) / 1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+    jsonData['two_swift'] = tempList
 
-for n in sizes:
-    a = list(range(n))
-    t1 = time.perf_counter_ns()
-    res = swift_swift(a, d)
-    t2 = time.perf_counter_ns()
-    print(f"n = {n:>6} | время = {(t2 - t1)/1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+    print(sep_print)
+    print(f"# Третий способ при {sizes} элементах массива при d=4 #")
 
-print(sep_print)
+    d = 4
+    tempList = {}
+    for n in sizes:
+        a = list(range(n))
+        t1 = time.perf_counter_ns()
+        res = swift_swift(a, d)
+        t2 = time.perf_counter_ns()
+        tempList[f"{n}"] = f"{(t2 - t1)}"
+        print(f"n = {n:>6} | время = {(t2 - t1) / 1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+    jsonData['three_swift'] = tempList
+
+    print(sep_print)
+    return jsonData
+
+
+def warmup():
+    try:
+        n = list(range(200))
+        for _ in range(500):
+            swift_loop(n, 12)
+            swift_temp(n, 3)
+            swift_swift(n, 4)
+    except Exception as e:
+        return "Прогрев не пройден. Ошибка в Python"
+
+    return "Прогрев пройден"
+
+
+def csvRead(jsonData):
+    import csv
+    with open("csv_result.csv", "w", encoding='utf-8') as f:
+        writer = csv.writer(f, delimiter=";")
+        writer.writerow(['Способ первый'])
+        writer.writerow(['Размер', "время в ns"])
+        for key, value in jsonData["first_swift"].items():
+            writer.writerow([key.strip(), value.strip()])
+        writer.writerow(['Способ второй'])
+        writer.writerow(['Размер', "время в ns"])
+        for key, value in jsonData["two_swift"].items():
+            writer.writerow([key.strip(), value.strip()])
+        writer.writerow(['Способ третий'])
+        writer.writerow(['Размер', "время в ns"])
+        for key, value in jsonData["three_swift"].items():
+            writer.writerow([key.strip(), value.strip()])
+
+
+if __name__ == "__main__":
+    print(warmup())
+    js = main_test(sizes)
+    csvRead(js)
