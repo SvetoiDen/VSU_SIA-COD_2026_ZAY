89220e7a32d699d8f56968a8fbb1fc4bbdc4e341
SvetoiDen <geymerworld123@gmail.com>
2026-09-23 15:32:55 +0300

    edit test.py - add funcs

--- task_1/test.py
+++ task_1/test.py
@@ -1,17 +1,38 @@
 from code_task import *
 import time
 
-sizes = [4, 32, 128, 512, 1024, 4096, 16384, 65536, 262144]
+
+def test_swift(func, arr, d):
+    # если первые d данные в конце массива - True
+    N = len(arr)
+    test_true = arr[:d]
+
+    res = func(arr, d)
+    return res[(N - d):] == test_true
+
+
+sizes = [4096, 16384, 65536, 262144, 500000, 1000000, 5_000_000, 10_000_000]
+sep_print = "# ==================================== #"
+
+
+def main_test_func(n, d):
+    print(sep_print)
+    print("Тест 1 - первый способ через замену")
+    print(test_swift(swift_temp, list(range(n)), d))
+    print("Тест 2 - второй способ через цикл в цикле")
+    print(test_swift(swift_loop, list(range(n)), d))
+    print("Тест 3 - третий способ через разрезы")
+    print(test_swift(swift_swift, list(range(n)), d))
+    print(sep_print)
 
 
 def main_test(sizes):
     jsonData = {}
 
-    sep_print = "# ==================================== #"
     print(sep_print)
     print("# Тестирование и время сдвига масссива на d позиции #")
     print(sep_print)
-    d = 3 # первый способ
+    d = 3  # первый способ
     print(f"# Первый способ при {sizes} элементах массива d={d} #")
 
     tempList = {}
@@ -21,11 +42,11 @@ def main_test(sizes):
         res = swift_temp(a, d)
         t2 = time.perf_counter_ns()
         tempList[f"{n}"] = f"{(t2 - t1)}"
-        print(f"n = {n:>6} | время = {(t2 - t1) / 1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+        print(f"n = {n:>12} | время = {(t2 - t1) / 1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
     jsonData['first_swift'] = tempList
 
     print(sep_print)
-    d = 4 # второй способ
+    d = 4  # второй способ
     print(f"# Второй способ при {sizes} элементах массива при d={d} #")
 
     tempList = {}
@@ -35,11 +56,11 @@ def main_test(sizes):
         res = swift_loop(a, d)
         t2 = time.perf_counter_ns()
         tempList[f"{n}"] = f"{(t2 - t1)}"
-        print(f"n = {n:>6} | время = {(t2 - t1) / 1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+        print(f"n = {n:>12} | время = {(t2 - t1) / 1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
     jsonData['two_swift'] = tempList
 
     print(sep_print)
-    d = 12 # третий способ
+    d = 12  # третий способ
     print(f"# Третий способ при {sizes} элементах массива при d={d} #")
 
     tempList = {}
@@ -49,24 +70,24 @@ def main_test(sizes):
         res = swift_swift(a, d)
         t2 = time.perf_counter_ns()
         tempList[f"{n}"] = f"{(t2 - t1)}"
-        print(f"n = {n:>6} | время = {(t2 - t1) / 1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
+        print(f"n = {n:>12} | время = {(t2 - t1) / 1_000_000:8.4f} ms | результат[:{d}+2] = {res[:(d + 2)]}")
     jsonData['three_swift'] = tempList
 
     print(sep_print)
     return jsonData
 
 
-def warmup():
-    try:
-        n = list(range(200))
-        for _ in range(500):
-            swift_loop(n, 12)
-            swift_temp(n, 3)
-            swift_swift(n, 4)
-    except Exception as e:
-        return "Прогрев не пройден. Ошибка в Python"
-
-    return "Прогрев пройден"
+# def warmup():
+#     try:
+#         n = list(range(200))
+#         for _ in range(500):
+#             swift_loop(n, 12)
+#             swift_temp(n, 3)
+#             swift_swift(n, 4)
+#     except Exception as e:
+#         return "Прогрев не пройден. Ошибка в Python"
+#
+#     return "Прогрев пройден"
 
 
 def csvRead(jsonData):
@@ -88,6 +109,6 @@ def csvRead(jsonData):
 
 
 if __name__ == "__main__":
-    print(warmup())
+    main_test_func(100, 6)
     js = main_test(sizes)
     csvRead(js)
